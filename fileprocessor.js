1321f8c6e3c2c34da8517d10ca111e97cefc1473
System Administrator <root@Jonass-Mac-mini.local>
2018-06-13 13:48:41 +0200

    fix

--- fileprocessor.js
+++ fileprocessor.js
@@ -50,7 +50,7 @@ class FileProcessor extends EventEmitter{
                 return 0;
             }
             files.forEach(file => {
-                fs.copyFile(homepath + '/empty/' + file, homepath, (err) => {
+                fs.copyFile(homepath + '/empty/' + file, homepath + file, (err) => {
                     if(err) return log.error(err);
                 });
             });
